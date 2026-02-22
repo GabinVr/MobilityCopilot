be84a23761ac03b7f10bbee005213a528faf705b
Julien <julien.excoffier@ecole.ensicaen.fr>
2026-02-22 06:39:38 +0100

    Enhance workflow by adding validator node and refining conditional edges for query validation

--- core/graph.py
+++ core/graph.py
@@ -10,7 +10,7 @@ from core.nodes.contradictor import contradictor_node
 from core.nodes.rag import rag_node
 from core.tools.tools_api_weather_now import geomet_mtl_weather_text_bundle
 from core.tools.tools_api_histo import geomet_mtl_history_global_tool
-
+from core.nodes.validator import execute_sql_node
 
 
 tools = [
@@ -38,15 +38,25 @@ def route_after_generator(state: CopilotState):
 
     if hasattr(last_message, "tool_calls") and last_message.tool_calls:
         return "call_tools"
+
+    if state.get("generated_query"):
+        return "validate_query"
+
     return "proceed_synthesis"
 
+def route_after_validator(state: CopilotState):
+    if state.get("query_error"):
+        return "sql_generator"
     
+    return "synthesis"
+
 workflow = StateGraph(CopilotState)
 
 workflow.add_node("retriever", rag_node)
 workflow.add_node("ambiguity_detector", ambiguity_node)
 workflow.add_node("user_interaction", user_interaction_node)
 workflow.add_node("sql_generator", sql_generator_node)
+workflow.add_node("validator", execute_sql_node)
 workflow.add_node("weather_tools", tool_node)
 workflow.add_node("synthesis", synthesis_node)
 workflow.add_node("contradictor", contradictor_node)
@@ -64,10 +74,17 @@ workflow.add_conditional_edges("ambiguity_detector", route_after_ambiguity, {
 
 workflow.add_conditional_edges("sql_generator", route_after_generator, {
     "call_tools": "weather_tools",
-    "proceed_synthesis": "synthesis"
+    "proceed_synthesis": "synthesis",
+    "validate_query": "validator"
 })
-workflow.add_edge("weather_tools", "sql_generator")
 
+
+workflow.add_conditional_edges("validator", route_after_validator, {
+    "sql_generator": "sql_generator",
+    "synthesis": "synthesis"
+})
+
+workflow.add_edge("weather_tools", "sql_generator")
 workflow.add_edge("user_interaction", END)
 workflow.add_edge("synthesis", "contradictor")
 workflow.add_edge("contradictor", END)
